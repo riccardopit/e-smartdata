58e835fab34c1a285f8f063f7a65b49e06d588aa
Riccardo Pittalis <102901401+riccardopit@users.noreply.github.com>
2025-01-05 20:23:19 +0100

    remove not necessary files

--- 350_Exercises_Python Programming_Mega_Pack_OOP/350_Exercises_Python Programming_Mega_Pack_OOP/6_transformation_of_data_structures/24_dict_comprehension/exercise_163.py
+++
@@ -1,9 +0,0 @@
-'''
-Using dict comprehension, create a dictionary that maps the numbers 1 to 7 into squares
-and print the result to the console.
-Expected result:
-{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49}
-'''
-
-result = {x: x**2 for x in range(1, 8)}
-print(result)
