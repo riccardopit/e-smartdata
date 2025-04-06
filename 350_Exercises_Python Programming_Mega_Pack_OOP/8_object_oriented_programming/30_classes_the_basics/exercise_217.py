6900de96e0169482b3d28a1bf14d5386bcca5405
Riccardo Pittalis <riccardopit.dev@gmail.com>
2025-04-06 11:34:17 +0200

    fix description

--- 350_Exercises_Python Programming_Mega_Pack_OOP/8_object_oriented_programming/30_classes_the_basics/exercise_217.py
+++ 350_Exercises_Python Programming_Mega_Pack_OOP/8_object_oriented_programming/30_classes_the_basics/exercise_217.py
@@ -3,8 +3,8 @@ The implementation of the Container class is given:
 class Container:
     """This is a Container class."""
 Display the value of the __module__ attribute of the Container class to the console.
-Note: The solution that the user provides is in a file named exercise.py, while the checking code
-(which is invisible to the user) is executed from a file named evaluate.py
+Note: The solution that the user provides is in a file named exercise.py,
+while the checking code (which is invisible to the user) is executed from a file named evaluate.py
 from the level where the Container class is imported. Therefore, instead of the name of the module __main__,
 the response will be the name of the module in which this class is implemented, exercise in this case.
 Expected result:
--- 350_Exercises_Python Programming_Mega_Pack_OOP/8_object_oriented_programming/30_classes_the_basics/exercise_219.py
+++ 350_Exercises_Python Programming_Mega_Pack_OOP/8_object_oriented_programming/30_classes_the_basics/exercise_219.py
@@ -4,8 +4,8 @@ class Container:
     """This is a Container class."""
 Create an instance of the Container class and assign to the container variable.
 Then print the __class__ attribute value of the container instance.
-Note: The solution that the user provides is in a file named exercise.py, while the checking code
-(which is invisible to the user) is executed from a file named evaluate.py
+Note: The solution that the user provides is in a file named exercise.py,
+while the checking code (which is invisible to the user) is executed from a file named evaluate.py
 from the level where the Container class is imported. Therefore, instead of the name of the module __main__,
 the response will be the name of the module in which this class is implemented, exercise in this case.
 Expected result:
