d8df953068ee8e791e947c00b0bb64c5ad6da9aa
Riccardo Pittalis <riccardopit.dev@gmail.com>
2025-04-27 15:51:31 +0200

    static method

---
+++ 350_Exercises_Python Programming_Mega_Pack_OOP/8_object_oriented_programming/38_static_method/exercise_284.py
@@ -0,0 +1,14 @@
+'''
+Define a Container class that has a static method (use the @staticmethod decorator)
+named get_current_time() returning the current time in the format '%H:%M:%S', e.g. '09:45:10'.
+Tip: Use the built-in time module.
+'''
+
+import time
+
+
+class Container:
+
+    @staticmethod
+    def get_current_time():
+        return time.strftime("%H:%M:%S", time.localtime())
---
+++ 350_Exercises_Python Programming_Mega_Pack_OOP/8_object_oriented_programming/38_static_method/exercise_286.py
@@ -0,0 +1,31 @@
+'''
+The Book class is implemented. Add a __repr__() method to the Book class
+that represents an instance of this class (see below).
+Then create an instance of the class named book1 passing the following arguments:
+title='Inferno'
+author='Dan Brown'
+In response, print the instance book1 to the console.
+Expected result:
+Book(title='Inferno', author='Dan Brown')
+'''
+
+import uuid
+
+
+class Book:
+
+    def __init__(self, title, author):
+        self.book_id = self.get_id()
+        self.title = title
+        self.author = author
+
+    @staticmethod
+    def get_id():
+        return str(uuid.uuid4().fields[-1])[:6]
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(title='{self.title}', author='{self.author}')"
+
+
+book1 = Book('Inferno', 'Dan Brown')
+print(book1)
